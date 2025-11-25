6504b13615423a2bcd10f7a8de2fbb820dcc156c
Rakhi Nathan <namakurakhi@gmail.com>
2025-11-26 06:47:30 +0700

    init: Setup Qt and repo structure

---
+++ logic.cpp
@@ -0,0 +1,9 @@
+#include "logic.h" // Mengimport struct dan fungsi yang telah dideklarasi
+#include <iostream>
+
+using namespace std;
+
+// Contoh (ntar hapus aja waktu commit)
+void contohFungsi(Penerbangan data[], int n){
+    cout << "Hello world";
+}
---
+++ logic.h
@@ -0,0 +1,10 @@
+#ifndef LOGIC_H // Mencegah double
+#define LOGIC_H
+
+#include "data_commons.h" // Mengimport struct
+#include <string>
+
+// Contoh (Ntar hapus aja waktu commit)
+void contohFungsi(Penerbangan data[], int n); // nama fungsi harus sama dengan logic.cpp
+
+#endif
