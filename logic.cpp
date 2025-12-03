a662ea45937865e48f625c1600fb13a85c7af7b7
azahwaen <azahwaezarmei@gmail.com>
2025-12-03 13:10:19 +0700

    feat : InputData, Filtering, Shuffling

--- logic.cpp
+++ logic.cpp
@@ -1,7 +1,27 @@
 #include "logic.h" // Mengimport struct dan fungsi yang telah dideklarasi
 #include <iostream>
+#include <ctime> // buat acha (Fisher–Yates Shuffle)
+#include <fstream> // Buat ifstream dll
 
 using namespace std;
+
+int inputData(Penerbangan data[], string filename){
+    ifstream file("flights.txt");
+    int n = 0;
+
+    while (file >> data[n].kode >> data[n].maskapai >> data[n].tujuan
+           >> data[n].jam >> data[n].harga >> data[n].jumlahTerisi)
+    {
+        for (int i = 0; i < TOTAL_KURSI; i++)
+            data[n].kursi[i] = "-";
+
+        n++;
+    }
+
+    file.close();
+    return n;
+}
+
 
 // 1. Searching Algorithm (Linear Search)
 int cariPenerbangan(const Penerbangan data[], int n, string keyword)
@@ -18,3 +38,42 @@ int cariPenerbangan(const Penerbangan data[], int n, string keyword)
     }
     return -1; // Jika tidak ketemu
 }
+
+
+// 2. FILTERING
+void filterTujuan(Penerbangan data[], int n, string tujuan){
+    cout << "Hasil Filter Tujuan " << tujuan << endl;
+
+    for (int i = 0; i < n; i++)
+        if (data[i].tujuan == tujuan)
+            cout << data[i].maskapai << " | "
+                 << data[i].jam << " | " << data[i].harga << endl;
+}
+
+// 3. Gacha or shuffle (Fisher–Yates Shuffle)
+void gachaKursi(Penerbangan &p){
+    srand(time(0));
+
+    int kosong[TOTAL_KURSI];
+    int totalKosong = 0;
+
+    for (int i = 0; i < TOTAL_KURSI; i++)
+        if (p.kursi[i] == "-")
+            kosong[totalKosong++] = i;
+
+    if (totalKosong == 0)
+    {
+        cout << "Kursi penuh!" << endl;
+        return;
+    }
+
+    int acak = rand() % totalKosong;
+    int hasil = kosong[acak];
+
+    p.kursi[hasil] = "BOOKED";
+    p.jumlahTerisi++;
+
+    cout << "Kursi Anda: " << hasil + 1 << endl;
+}
+
+
