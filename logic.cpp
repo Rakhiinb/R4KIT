3d00fc0fc3c00f0c39f6d1ae058f5ccc2b32807b
atikashaf <atikashafiraa13@gmail.com>
2025-12-08 20:43:33 +0700

    rakhi bener ngga

--- logic.cpp
+++ logic.cpp
@@ -2,18 +2,23 @@
 #include <iostream>
 #include <fstream>
 #include <QDebug>
+#include <string>
 
 using namespace std;
 
 // Menghitung jumlah kursi yang sudah dibooking dalam pesawat tertentu
-int jumlahBooking(string kodePesawat){
+int jumlahBooking(string kodePesawat)
+{
     ifstream file("bookings.txt");
-    if(!file.is_open()) return 0;
+    if (!file.is_open())
+        return 0;
     string nama, kode;
     int kursi;
-    int count=0;
-    while(file >> nama >> kode >> kursi){
-        if(kode == kodePesawat){
+    int count = 0;
+    while (file >> nama >> kode >> kursi)
+    {
+        if (kode == kodePesawat)
+        {
             count++;
         }
     }
@@ -22,37 +27,51 @@ int jumlahBooking(string kodePesawat){
 }
 
 // Memperbarui data terbaru di file flights.txt
-void loadDataPenerbangan(Penerbangan data[], int &jumlahData) {
+void loadDataPenerbangan(Penerbangan data[], int &jumlahData)
+{
     ifstream file("flights.txt");
     jumlahData = 0;
 
-    if (!file.is_open()) {
+    if (!file.is_open())
+    {
         qDebug() << "File flights.txt Gagal Dibuka!";
         return;
     }
 
     qDebug() << "File Berhasil Dibuka!";
-    int tempJumlahTerisi; 
+    int tempJumlahTerisi;
 
-    while (file >> data[jumlahData].kode 
-                >> data[jumlahData].maskapai 
-                >> data[jumlahData].tujuan 
-                >> data[jumlahData].jam 
-                >> data[jumlahData].harga 
-                >> tempJumlahTerisi) { // Baca angka 0 di file txt
-        
+    while (file >> data[jumlahData].kode >> data[jumlahData].maskapai >> data[jumlahData].tujuan >> data[jumlahData].jam >> data[jumlahData].harga >> tempJumlahTerisi)
+    { // Baca angka 0 di file txt
+
         data[jumlahData].jumlahTerisi = jumlahBooking(data[jumlahData].kode);
-        
+
         jumlahData++;
-        if (jumlahData >= MAX_PENERBANGAN) break;
+        if (jumlahData >= MAX_PENERBANGAN)
+            break;
     }
+    file.close();
+}
+
+void simpanPenumpang(const Penumpang &p)
+{
+    ofstream file("bookings.txt", ios::app);
+
+    if (!file.is_open())
+    {
+        cout << "Gagal membuka bookings.txt\n";
+        return;
+    }
+
+    file << p.nama << " " << p.kodePenerbangan << " " << p.nomorKursi << "\n";
+
     file.close();
 }
 
 // 1. Searching Algorithm (Linear Search)
 void cariPenerbangan(const Penerbangan data[], int n, string keyword, Penerbangan hasil[], int &nHasil)
 {
-    nHasil=0;
+    nHasil = 0;
     for (int i = 0; i < n; i++)
     {
         // Cari berdasarkan kota tujuan, kode penerbangan, atau maskapai
@@ -64,4 +83,46 @@ void cariPenerbangan(const Penerbangan data[], int n, string keyword, Penerbanga
             nHasil++;
         }
     }
+}
+
+void deduplikasi() {
+    Penumpang arr[500];
+    int n = 0;
+
+    ifstream fin("bookings.txt");
+    while (fin >> arr[n].nama >> arr[n].kodePenerbangan >> arr[n].nomorKursi)
+    {
+        n++;
+    }
+    fin.close();
+
+    Penumpang bersih[500];
+    int m = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        bool duplikat = false;
+
+        for (int j = 0; j < m; j++)
+        {
+            if (arr[i].kodePenerbangan == bersih[j].kodePenerbangan &&
+                arr[i].nomorKursi == bersih[j].nomorKursi)
+            {
+                duplikat = true;
+                break;
+            }
+        }
+
+        if (!duplikat)
+        {
+            bersih[m] = arr[i];
+            m++;
+        }
+    }
+    ofstream fout("bookings.txt");
+    for (int i = 0; i < m; i++)
+    {
+        fout << bersih[i].nama << " " << bersih[i].kodePenerbangan << " "<< bersih[i].nomorKursi << "\n";
+    }
+    fout.close();
 }
