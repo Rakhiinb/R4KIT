984280f399acc49620ffa5c20c11bf4770081b2e
Rakhi Nathan <namakurakhi@gmail.com>
2025-12-09 16:22:41 +0700

    fix: Fixing logic to connect with Qt

--- logic.cpp
+++ logic.cpp
@@ -5,34 +5,24 @@
 
 using namespace std;
 
-int inputData(Penerbangan data[], string filename){
-    ifstream file("flights.txt");
-    int n = 0;
+// Gacha Algorithm helper
+bool cekKursiTerisi(string kodePesawat, int noKursi) {
+    ifstream file("bookings.txt");
+    if (!file.is_open()) return false; // Belum ada yang booking
 
-    while (file >> data[n].kode >> data[n].maskapai >> data[n].tujuan
-           >> data[n].jam >> data[n].harga >> data[n].jumlahTerisi)
-    {
-        for (int i = 0; i < TOTAL_KURSI; i++)
-            data[n].kursi[i] = "-";
-
-        n++;
+    string nama, kode;
+    int kursi;
+    
+    // Baca file booking satu per satu
+    while (file >> nama >> kode >> kursi) {
+        // Kalau ketemu kode pesawat SAMA dan kursi SAMA -> berarti TERISI
+        if (kode == kodePesawat && kursi == noKursi) {
+            return true; 
+        }
     }
-
     file.close();
-    return n;
+    return false;
 }
-
-string konversiWaktu(string jam) {
-    int angkaJam = stoi(jam.substr(0, 2));
-
-    if (angkaJam >= 5 && angkaJam <= 11)
-        return "pagi";
-    else if (angkaJam >= 12 && angkaJam <= 17)
-        return "siang";
-    else
-        return "malam";
-}
-
 // 1. Searching Algorithm (Linear Search)
 int cariPenerbangan(const Penerbangan data[], int n, string keyword)
 {
@@ -49,52 +39,77 @@ int cariPenerbangan(const Penerbangan data[], int n, string keyword)
     return -1; // Jika tidak ketemu
 }
 
+// 2. Filtering Algorithm
+void filterWaktu(const Penerbangan sumber[], int nSumber, 
+                 Penerbangan hasil[], int &nHasil, 
+                 int kategoriWaktu) {
+    
+    nHasil = 0;
 
-// 2. FILTERING
-int filterPenerbangan(Penerbangan data[], int n, Penerbangan hasil[], string waktuInput, int hargaMax) {
-    int jumlahHasil = 0;
+    for (int i = 0; i < nSumber; i++) {
+        // 0 = Semua Waktu
+        if (kategoriWaktu == 0) {
+            hasil[nHasil] = sumber[i];
+            nHasil++;
+            continue; // Lanjut ke data berikutnya
+        }
 
-    for (int i = 0; i < n; i++) {
-        string kategori = konversiWaktu(data[i].jam);
+        // Ambil 2 angka depan lalu ubah jadi integer.
+        int jamInt = std::stoi(sumber[i].jam.substr(0, 2)); 
+        bool lolos = false;
 
-        if (kategori == waktuInput && data[i].harga <= hargaMax) {
-            hasil[jumlahHasil].kode = data[i].kode;
-            hasil[jumlahHasil].maskapai = data[i].maskapai;
-            hasil[jumlahHasil].tujuan = data[i].tujuan;
-            hasil[jumlahHasil].jam = data[i].jam;
-            hasil[jumlahHasil].harga = data[i].harga;
-            jumlahHasil++;
+        // 1 = PAGI (00:00 sampai 10:59)
+        if (kategoriWaktu == 1) { 
+            if (jamInt >= 0 && jamInt < 11) lolos = true;
+        }
+        // 2 = SIANG (11:00 sampai 17:59)
+        else if (kategoriWaktu == 2) { 
+            if (jamInt >= 11 && jamInt < 18) lolos = true;
+        }
+        // 3 = MALAM (18:00 sampai 23:59)
+        else if (kategoriWaktu == 3) { 
+            if (jamInt >= 18 && jamInt <= 23) lolos = true;
+        }
+
+        if (lolos) {
+            hasil[nHasil] = sumber[i];
+            nHasil++;
+        }
+        
+    }
+}
+
+// 3. Gacha or shuffle (Fisher–Yates Shuffle)
+int gachaKursi(string kodePesawat) {
+    // Cek apakah pesawat sudah penuh (50 orang)
+    if (jumlahBooking(kodePesawat) >= TOTAL_KURSI) {
+        return -1;
+    }
+
+    // Siapkan array kursi 1 sampai 50
+    int kolamKursi[TOTAL_KURSI];
+    for (int i = 0; i < TOTAL_KURSI; i++) {
+        kolamKursi[i] = i + 1;
+    }
+
+    // Fisher-Yates Algorithm
+    for (int i = TOTAL_KURSI - 1; i > 0; i--) {
+        int j = rand() % (i + 1); // Pilih index acak
+        
+        // Swap
+        int temp = kolamKursi[i];
+        kolamKursi[i] = kolamKursi[j];
+        kolamKursi[j] = temp;
+    }
+
+    // Cek kursi terisi atau tidak
+    for (int i = 0; i < TOTAL_KURSI; i++) {
+        int calonKursi = kolamKursi[i];
+        
+        if (!cekKursiTerisi(kodePesawat, calonKursi)) {
+            return calonKursi; 
         }
     }
 
-    return jumlahHasil;
+    return -1; 
 }
-// 3. Gacha or shuffle (Fisher–Yates Shuffle)
-int gachaKursi(Penerbangan &p)
-{
-    int kosong[TOTAL_KURSI];
-    int totalKosong = 0;
-
-    for (int i = 0; i < TOTAL_KURSI; i++)
-        if (p.kursi[i] == "-")
-            kosong[totalKosong++] = i;
-
-    if (totalKosong == 0)
-        return -1;
-
-    for (int i = totalKosong - 1; i > 0; i--) {
-        int j = rand() % (i + 1);
-        int temp = kosong[i];
-        kosong[i] = kosong[j];
-        kosong[j] = temp;
-    }
-    int hasil = kosong[0];
-
-    p.kursi[hasil] = "BOOKED";
-    p.jumlahTerisi++;
-
-    return hasil + 1;
-}
-
-
-
